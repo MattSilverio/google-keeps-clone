46510524bc0b86ff2ae5d140ee04932ede04b5b6
Matheus Silverio <matheusphillipo@gmail.com>
2023-05-21 03:29:24 -0300

    init App component

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import "./reset.css"
+  import NavBar from "./components/NavBar/NavBar.svelte";
+  
+</script>
+
+<style>
+  
+</style>
+  <NavBar />
+ <!-- Fazer GridView e Aside bar -->
+ 
