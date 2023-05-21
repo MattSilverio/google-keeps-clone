1f8c4b7bbee8b02e72befd996716cb69b4795dd0
Matheus Silverio <matheusphillipo@gmail.com>
2023-05-21 03:27:56 -0300

    AsideBars Components

---
+++ src/components/AsideBar/AsideBar.svelte
@@ -0,0 +1,80 @@
+<script>
+import FaRegLightbulb from 'svelte-icons/fa/FaRegLightbulb.svelte'
+import FaRegBell from 'svelte-icons/fa/FaRegBell.svelte'
+import FaRegTrashAlt from 'svelte-icons/fa/FaRegTrashAlt.svelte'
+import FaArchive from 'svelte-icons/fa/FaArchive.svelte'
+
+
+
+
+</script>
+
+<style>
+  ul{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  div{
+    width: 48px;
+    height: 24px;
+  }
+
+  li{
+    display: flex;
+    align-items: center;
+    padding: 8px 16px;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+
+  li:hover{
+    background-color: #e8eaed;
+  }
+
+  a{
+    text-decoration: none;
+    color: #5f6368;
+  }
+
+  .aside-bar{
+    margin-top: 20px;
+    width: 200px;
+    height: 200px;
+    border-radius: 8px;
+    background-color: #f1f3f4;
+    padding: 18px;
+    z-index: 999;
+  }
+</style>
+
+<aside class="aside-bar">
+  <ul>
+    <li>
+      <div>
+        <FaRegLightbulb />
+      </div>
+      <a href="/">Notas</a>
+    </li>
+    <li>
+      <div>
+        <FaRegBell/>
+      </div>
+      <a href="/reminders">Lembretes</a>
+    </li>
+    <li>
+      <div>
+        <FaArchive/>
+      </div>
+      <a href="/archive">Arquivo</a>
+    </li>
+    <li>
+      <div>
+        <FaRegTrashAlt/>
+      </div>
+      <a href="/trash">Lixeira</a>
+    </li>
+  </ul>
+</aside>
+
---
+++ src/components/AsideBarReduced/AsideBarReduced.svelte
@@ -0,0 +1,66 @@
+<script>
+  import FaRegLightbulb from 'svelte-icons/fa/FaRegLightbulb.svelte'
+  import FaRegBell from 'svelte-icons/fa/FaRegBell.svelte'
+  import FaRegTrashAlt from 'svelte-icons/fa/FaRegTrashAlt.svelte'
+  import FaArchive from 'svelte-icons/fa/FaArchive.svelte'
+
+  
+  </script>
+  
+  <style>
+   
+    div{
+      width: 100%;
+      height: 24px;
+    }
+
+    div:hover{
+      background-color: #e8eaed;
+      border-radius: 8px;
+    }
+  
+    li{
+      display: flex;
+      align-items: center;
+      padding: 8px 16px;
+      border-radius: 8px;
+      cursor: pointer;
+    }
+  
+    .aside-bar{
+      margin-top: 20px;
+      width: 50px;
+      margin-left: 16px;
+      height: 200px;
+      background-color: transparent;
+      padding: 18px;
+      z-index: 999;
+    }
+  </style>
+  
+  <aside class="aside-bar">
+    <ul>
+      <li>
+        <div>
+          <a href="/"><FaRegLightbulb /></a>
+        </div>
+      </li>
+      <li>
+        <div>
+          <a href="/reminders"><FaRegBell/></a>
+        </div>
+      </li>
+      <li>
+        <div>
+          <a href="/archive"><FaArchive/></a>
+        </div>
+      </li>
+      <li>
+        <div>
+          <a href="/trash"><FaRegTrashAlt/></a>
+        </div>
+      </li>
+    </ul>
+  </aside>
+  
+  
