dd4d473725f956692c86a8b2482c454efc930723
Matheus Silverio <matheusphillipo@gmail.com>
2023-05-21 03:29:02 -0300

    main route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,33 @@
-<script>
-  import NavBar from "../components/NavBar/NavBar.svelte";
+<script lang="ts">
+  import App from "../App.Svelte";
+	import { getNotesList } from "../services/notes";
+  import type { Note } from "../interfaces/notes";
+  import	{onMount} from "svelte";
+
+  let notes: Note[];
+  onMount(async () => {
+    const result = await getNotesList();
+    notes = result
+    
+  });
 </script>
 
-  <NavBar />
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: 200px 1fr 1fr 1fr 1fr;
+    gap: 16px;
+  }
+</style>
 
+<App/>
+<!-- <div class="container">
+
+  {#if notes}
+  <ul>
+    {#each notes as note}
+      <Card note={note} />
+    {/each}
+  </ul>
+{/if}
+</div> -->
