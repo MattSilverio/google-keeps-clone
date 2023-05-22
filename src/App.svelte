2b490e65ea3fbe53bbb5fb7451a78eb199223c3a
Matheus Silverio <matheusphillipo@gmail.com>
2023-05-21 22:50:28 -0300

    Created AppViewDefault

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import "./reset.css"
-  import NavBar from "./components/NavBar/NavBar.svelte";
+  import AppViewDefault from "./components/AppViewDefault/AppViewDefault.svelte";
   
 </script>
 
 <style>
   
 </style>
-  <NavBar />
- <!-- Fazer GridView e Aside bar -->
+  
+<AppViewDefault/>
+ 
  
---
+++ src/components/AppViewDefault/AppViewDefault.svelte
@@ -0,0 +1,30 @@
+<script>
+  import GridView from "../GridView/GridView.svelte";
+  import NavBar from "../NavBar/NavBar.svelte";
+
+</script>
+
+<style>
+  .conteiner{
+    display: grid;
+    grid-template-columns: 200px 1fr;;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+  }
+
+  .cards-conteiner{
+    grid-column: 2;
+    position: relative;
+    top: 4rem;
+  }
+</style>
+
+<div class="conteiner">
+  <NavBar/>
+  <div class='cards-conteiner'>
+    <GridView/>
+    
+  </div>
+
+</div>
