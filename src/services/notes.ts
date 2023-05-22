ecf8d0f7a8a3cb675eb257c8be3b27e32741529d
Matheus Silverio <matheusphillipo@gmail.com>
2023-05-21 22:49:32 -0300

    add notes services

--- src/services/notes.ts
+++ src/services/notes.ts
@@ -4,3 +4,23 @@ export async function getNotesList() {
 	const response = await api.get('/notes');
 	return response.data;
 }
+
+export async function getNoteById(id: string) {
+	const response = await api.get(`/notes/${id}`);
+	return response.data;
+}
+
+export async function createNewNote() {
+	const response = await api.post('/notes');
+	return response.data;
+}
+
+export async function deleteNoteById(id: string) {
+	const response = await api.delete(`/notes/${id}`);
+	return response.data;
+}
+
+export async function updateNoteById(id: string, data: any) {
+	const response = await api.put(`/notes/${id}`, data);
+	return response.data;
+}
